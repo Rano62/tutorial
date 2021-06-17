5669508ccc7f0895bdf0ab0a2ff95f0353dea902
rano62 <rano62ac26@hotmail.com>
2021-06-17 23:19:28 +0200

    basic react

---
+++ src/books.js
@@ -0,0 +1,21 @@
+
+ export const books=[
+   {
+     id:1,
+    author : 'Dav Pilkey',
+    title :'Dog Man: Mothering Heights: From the Creator of Captain Underpants (Dog Man #10)',
+    img:'https://m.media-amazon.com/images/I/71QuWEIWHlL._AC_UY218_.jpg',
+    },
+ {
+   id:2,
+   img:'https://images-eu.ssl-images-amazon.com/images/I/816TRYroBQS._AC_UL200_SR200,200_.jpg',
+   title:'Idéfix et les Irréductibles - BD dérivée de la série',
+   author:'René Goscinny',
+ },
+ {
+   id:3,
+    author : 'Roger Hargreaves',
+    title :'Monsieur Madame - Mon papa',
+    img:'https://images-eu.ssl-images-amazon.com/images/I/71vRPN%2BuqWL._AC_UL200_SR200,200_.jpg',
+    },
+];
--- src/index.js
+++ src/index.js
@@ -4,27 +4,9 @@ import ReactDom from 'react-dom'
 import './index.css';
 
 //setup vars
- const books=[
-   {
-     id:1,
-    author : 'Dav Pilkey',
-    title :'Dog Man: Mothering Heights: From the Creator of Captain Underpants (Dog Man #10)',
-    img:'https://m.media-amazon.com/images/I/71QuWEIWHlL._AC_UY218_.jpg',
-    },
- {
-   id:2,
-   img:'https://images-eu.ssl-images-amazon.com/images/I/816TRYroBQS._AC_UL200_SR200,200_.jpg',
-   title:'Idéfix et les Irréductibles - BD dérivée de la série',
-   author:'René Goscinny',
- },
- {
-   id:3,
-    author : 'Roger Hargreaves',
-    title :'Monsieur Madame - Mon papa',
-    img:'https://images-eu.ssl-images-amazon.com/images/I/71vRPN%2BuqWL._AC_UL200_SR200,200_.jpg',
-    },
-];
+import {books} from './books'
 
+import Book from './Book'
 
 function BookList(){
  return (
@@ -39,23 +21,7 @@ function BookList(){
  );
 }
 
-const Book = (props)=>{
-  const {img, title,author}=props.book;
-  const clickHandler = () => {
-    alert('hello world');
-  }
 
-  return (
-    <article className='book'>
-      <img src={img} alt=""/>
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-      <button type='button' onClick={clickHandler}>
-      reference example 
-      </button>
-    </article>
-    );
-};
 
 ReactDom.render(<BookList/>,document.getElementById('root')
 );
